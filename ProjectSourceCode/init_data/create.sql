5951c08639e28941809fedf9bba8278697f393cf
root <root@AsusGo.>
2025-04-01 13:31:55 -0600

    Updated databaase

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,5 +1,89 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE apf_users (
-    username    VARCHAR(50) PRIMARY KEY,
-    password    VARCHAR(60) NOT NULL
+-- DROP TABLE IF EXISTS users;
+-- CREATE TABLE apf_users (
+--     username    VARCHAR(50) PRIMARY KEY,
+--     password    VARCHAR(60) NOT NULL
+-- );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, --hashed passsword
+    username VARCHAR(50),
+    first_name VARCHAR(25),
+    last_name VARCHAR(25)
+);
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(50),
+    postal_code VARCHAR(10) NOT NULL,
+    country VARCHAR(100) DEFAULT 'USA',
+    is_default BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE vehicles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    make VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    year INTEGER NOT NULL
+);
+
+CREATE TABLE parts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    category VARCHAR(100)
+);
+
+CREATE TABLE part_compatibility (
+    id SERIAL PRIMARY KEY,
+    part_id INTEGER REFERENCES parts(id) ON DELETE CASCADE,
+    make VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    year_start INTEGER, -- year that it is first compatitble for
+    year_end INTEGER -- year that it is last compatitble for
+);
+
+CREATE TABLE vendors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    website VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE pricing (
+    id SERIAL PRIMARY KEY,
+    part_id INTEGER REFERENCES parts(id) ON DELETE CASCADE,
+    vendor_id INTEGER REFERENCES vendors(id),
+    price DECIMAL(10, 2) NOT NULL,
+    -- shipping DECIMAL(10, 2),
+    -- url VARCHAR(255),
+    UNIQUE(part_id, vendor_id)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    vehicle_id INTEGER REFERENCES vehicles(id),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(25)
+);
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    part_id INTEGER REFERENCES parts(id),
+    quantity INTEGER DEFAULT 1,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+-- Repair history
+CREATE TABLE repairs (
+    id SERIAL PRIMARY KEY,
+    vehicle_id INTEGER REFERENCES vehicles(id) ON DELETE CASCADE,
+    part_id INTEGER REFERENCES parts(id),
+    repair_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
