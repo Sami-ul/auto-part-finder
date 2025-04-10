88b1b78575cbae76a48aa65a08d1ac76a78129ae
Sabir Saklayen <sasa4287@colorado.edu>
2025-04-09 18:23:13 -0600

    Added DISTINCT to SELECT queries

--- ProjectSourceCode/init_data/create_vehicles_db.sql
+++ ProjectSourceCode/init_data/create_vehicles_db.sql
@@ -15,15 +15,15 @@ COPY vehicle_data FROM '/home/node/app/vehicle_data.csv' WITH (FORMAT CSV, HEADE
 -- To get vehicle Years
 -- @make in WHERE clause should contain the value of first dropdown box for vehicle Make
 -- @year in WHERE clause should be >= 1960 to ensure threshold
--- SELECT year FROM vehicle_data WHERE make = 'Honda' AND year >= 1960 ORDER BY year DESC;
+-- SELECT DISTINCT year FROM vehicle_data WHERE make = 'Honda' AND year >= 1960 ORDER BY year DESC;
 
 -- To get vehicle Model
 -- @make in WHERE clause should contain the value of first dropdown box for vehicle Make (String)
 -- @year in WHERE clause should contain the value of second dropdown box for vehicle Year (Integer)
--- SELECT model FROM vehicle_data WHERE make = 'Honda' AND year = 2020 ORDER BY model;
+-- SELECT DISTINCT model FROM vehicle_data WHERE make = 'Honda' AND year = 2020 ORDER BY model;
 
 -- To get vehicle Engine
 -- @make in WHERE clause should contain the value of first dropdown box for vehicle Make (String)
 -- @year in WHERE clause should contain the value of second dropdown box for vehicle Year (Integer)
 -- @model in WHERE clause should contain the value of third dropdown box for vehicle Model (String)
--- SELECT engine FROM vehicle_data WHERE make = 'Honda' AND year = 2020 AND model='CR-V' ORDER BY engine;
+-- SELECT DISTINCT engine FROM vehicle_data WHERE make = 'Honda' AND year = 2020 AND model='CR-V' ORDER BY engine;
